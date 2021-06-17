e9509515ea42365addb4b146c654aa39a45547ee
Jesus Samitier <contacto@jasamitier.com>
2021-06-17 12:50:26 +0200

    add auto-generated id to Person

--- src/models/person.rs
+++ src/models/person.rs
@@ -1,7 +1,29 @@
 use nanoserde::{DeJson, SerJson};
+use rand::prelude::*;
 
 #[derive(SerJson, DeJson, Clone, Debug)]
 pub struct Person {
-    pub name: String,
-    pub age: u8,
+    name: String,
+    age: u8,
+    id: u16,
+}
+
+impl Person {
+    pub fn new(name: String, age: u8) -> Self {
+        let id: u16 = random();
+        Self { name, age, id }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_creates_a_new_person() {
+        let person = Person::new("Person".to_string(), 29);
+        assert_eq!(person.age, 29);
+        assert_eq!(person.name, "Person");
+        assert!(person.id >= u16::MIN && person.id <= u16::MAX);
+    }
 }
